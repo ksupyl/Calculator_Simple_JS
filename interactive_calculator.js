c7b095339d77fc19e020ccfd85f49e7528d66a63
Ksenia <Xeonica01owl@gmail.com>
2024-05-13 20:03:09 +0300

    Commit 4

--- interactive_calculator.js
+++ interactive_calculator.js
@@ -2,9 +2,18 @@ function addToDisplay(value) {
     document.getElementById('display').value += value;
 }
 
+function deleteChar(input) {
+    if(input.value.length == 1) {
+        input.value = 0;
+    } else {
+        input.value = input.value.substring(0, input.value.length - 1);
+    }
+}
+
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
+
 
 function calculateResult() {
     try {
